e14009ed26fb9447bb22f165695a570a19153ba7
Polyakov Igor <polykovio@mail.ru>
2016-08-12 14:22:33 +0300

    Added <head> information, stylesheet reset.css and images

---
+++ index.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<base href="" /> <!--CHANGE_ME (to "http://SITE_NAME.ru/") -->
+		<meta charset="utf-8" />
+		<meta name="description" content="Торты на заказ в Нижнем Новгороде. Экслюзивные домашние торты для любого торжества. Только натуральные ингредиенты." />
+		<meta name="keywords" content="торт,заказать торт,торт на заказ,tort,торт на день рождения,торт на свадьбу,торт на корпоратив,торт на крестины,торт жене,торт мужу,торт ребенку,торт детям,торт на совершеннолетие,заказать торт в Нижнем,торт на годовщину,праздничный торт,торт на новый год,торт сыну,торт дочери,домашний торт,торт на заказ,заказать торт на свадьбу" />
+		<link rel="icon" href="images/icon/cake.png" type="image/png" />
+		<link rel="stylesheet" href="reset.css" type="text/css" />
+		<title>Торт на заказ</title> <!--CHANGE_ME -->
+	</head>
+	
+	<!--[if lt IE 9]>
+		<script src="html5shiv.js"></script>
+	<![endif]-->
+	<!--[if lte IE 8]>
+		<scrypt src="respond.js"></scrypt>
+	<![endif]-->
+	
+	<body>
+		<?php 
+			echo "Hello world!";
+		?>
+	</body>
+	
+</html>
