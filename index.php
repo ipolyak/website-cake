71c240c50a99025164571507d4ec57764f7075a2
Polyakov Igor <polykovio@mail.ru>
2016-08-20 18:49:47 +0300

    Added site navigation, head info and base.css stylesheet

--- index.php
+++ index.php
@@ -6,18 +6,34 @@
 		<meta name="description" content="Торты на заказ в Нижнем Новгороде. Экслюзивные домашние торты для любого торжества. Только натуральные ингредиенты." />
 		<meta name="keywords" content="торт,заказать торт,торт на заказ,tort,торт на день рождения,торт на свадьбу,торт на корпоратив,торт на крестины,торт жене,торт мужу,торт ребенку,торт детям,торт на совершеннолетие,заказать торт в Нижнем,торт на годовщину,праздничный торт,торт на новый год,торт сыну,торт дочери,домашний торт,торт на заказ,заказать торт на свадьбу" />
 		<link rel="icon" href="images/icon/cake.png" type="image/png" />
-		<link rel="stylesheet" href="reset.css" type="text/css" />
+		<link rel="stylesheet" href="CSS/reset.css" type="text/css" />
+		<link rel="stylesheet" href="CSS/base.css" type="text/css" />
 		<title>Торт на заказ</title> <!--CHANGE_ME -->
 	</head>
 	
 	<!--[if lt IE 9]>
-		<script src="html5shiv.js"></script>
+		<script src="JS/html5shiv.js"></script>
 	<![endif]-->
 	<!--[if lte IE 8]>
-		<scrypt src="respond.js"></scrypt>
+		<scrypt src="JS/respond.js"></scrypt>
 	<![endif]-->
 	
 	<body>
+		<header>
+			<span class="Sniglet">Sweet Dream</span>
+		</header>
+		
+		<nav class="LeftBlock">
+			<ul>
+				<li><a href="">Главная</a></li>
+				<li><a href="">Состав тортов</a></li>
+				<li><a href="">Галерея</a></li>
+				<li><a href="">Заказать торт</a></li>
+				<li><a href="">Контакты</a></li>
+				<li><a href="">Отзывы</a></li>
+			</ul>
+		</nav>
+		
 		<?php 
 			echo "Hello world!";
 		?>
